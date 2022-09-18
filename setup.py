cd81ed0c9aeac9b3d5fb2e912672241557b4febf
Wendirad Demelash <wendiradame@gmail.com>
2022-09-18 17:15:32 +0300

    (feat) Add get_requirements function

--- setup.py
+++ setup.py
@@ -1,26 +1,35 @@
 from setuptools import find_packages, setup
 
+
 def get_discription():
     """
     Read full description from `README.md`
     """
 
-    with open('README.md', 'r') as file:
-        return file.read()
+    with open("README.md", "r") as long_description:
+        return long_description.read()
+
+
+def get_requirements():
+    """
+    Return all requirements from `requirements.txt`
+    """
+
+    with open("requirements.txt", "r") as requirements:
+        return [line.strip() for line in requirements.readlines()]
+
 
 setup(
     name="yenepay",
     version="0.0.1",
     author="Wendirad Demelash",
-    author_email='wendiradame@backostech.com',
+    author_email="wendiradame@backostech.com",
     description="Python SDK for YenePay payment integration",
     long_description=get_discription(),
-    long_description_content_type='text/markdown',
-    packages=find_packages(exclude=("tests", "tests.*", 'examples.*', 'docs')),
+    long_description_content_type="text/markdown",
+    packages=find_packages(exclude=("tests", "tests.*", "examples.*", "docs")),
     url="https://github.com/backostech/yenepay.sdk.python",
     license="MIT",
-    python_requires='>=3.8',
-    install_requires=[
-        "requests==2.22.0"
-    ]
+    python_requires=">=3.8",
+    install_requires=get_requirements(),
 )
