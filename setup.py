3d9843b1c544bc33195596366b4696fbaf47fc38
Wendirad Demelash <wendiradame@gmail.com>
2022-09-23 15:24:07 +0300

    (fix)(feat) Change get_version and add classifiers

--- setup.py
+++ setup.py
@@ -1,4 +1,9 @@
+import pathlib
+import re
+
 from setuptools import find_packages, setup
+
+WORK_DIR = pathlib.Path(__file__).parent
 
 
 def get_discription():
@@ -10,18 +15,22 @@ def get_discription():
         return long_description.read()
 
 
-def get_requirements():
+def get_version():
     """
-    Return all requirements from `requirements.txt`
+    Read version
     """
-
-    with open("requirements.txt", "r") as requirements:
-        return [line.strip() for line in requirements.readlines()]
+    txt = (WORK_DIR / "yenepay" / "__init__.py").read_text("utf-8")
+    try:
+        return re.findall(
+            r"^__version__\s+=\s+['|\"]([^']+)['|\"]\r?$", txt, re.M
+        )[0]
+    except IndexError:
+        raise RuntimeError("Unable to determine version.")
 
 
 setup(
     name="yenepay",
-    version="0.2.0",
+    version=get_version(),
     author="Backos Technologies",
     author_email="info@backostech.com",
     description=(
@@ -34,5 +43,23 @@ setup(
     url="https://github.com/backostech/yenepay.sdk.python",
     license="MIT",
     python_requires=">=3.8",
-    install_requires=get_requirements(),
+    install_requires=[
+        "requests==2.28.1",
+    ],
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Environment :: Console",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Education",
+        "Intended Audience :: End Users/Desktop",
+        "Intended Audience :: Financial and Insurance Industry",
+        "License :: OSI Approved :: MIT License",
+        "Natural Language :: English",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ],
 )
