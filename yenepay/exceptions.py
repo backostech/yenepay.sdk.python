bc72801f1ecc06f5449ecff4a4d8367809c7e819
Wendirad Demelash <wendiradame@gmail.com>
2022-10-03 00:04:13 +0300

    (feat) Add exception classes

--- yenepay/exceptions.py
+++ yenepay/exceptions.py
@@ -5,3 +5,11 @@ YenePay exceptions
 
 class CheckoutError(Exception):
     """Exception for checkout errors."""
+
+
+class PDTError(Exception):
+    """Exception for PDT errors."""
+
+
+class IPNError(Exception):
+    """Exception for IPN errors."""
